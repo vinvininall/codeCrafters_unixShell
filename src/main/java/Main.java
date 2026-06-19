7fde6fd3653d5b7afbc66eef1c0892e488b34931
Vinayak <vinayaksingal20148@gmail.com>
2026-06-19 15:57:41 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
 
+// public class Main {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         while(true){
+//             System.out.print("$ ");
+//             System.out.flush();
+//             String input = sc.nextLine();
+//             if(input.equals("exit 0")){
+//                 break;
+//             }
+//             System.out.println(input);
+//         }
+//     }
+// }
+
+
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        while(true){
-            System.out.print("$ ");
-            System.out.flush();
-            String input = sc.nextLine();
-            if(input.equals("exit 0")){
-                break;
-            }
-            System.out.println(input);
-        }
+    public static void main(String[] args) throws Exception {
+        System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input + ": command not found");
     }
 }
