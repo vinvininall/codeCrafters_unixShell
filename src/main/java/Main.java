6debccc56ce29a863d1e0eb9f2ceb4fa77f0ad5e
Vinayak <vinayaksingal20148@gmail.com>
2026-06-19 16:04:54 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,12 @@ public class Main {
             if(input.equals("exit")){
                 break;
             }
+            else if(input.startsWith(("echo"))){
+                System.out.println(input.substring(5));
+            }
+            else{
             System.out.println(input + ": command not found");
+            }
         }
     }
 }
